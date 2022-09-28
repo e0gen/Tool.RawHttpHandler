06dc13fb0b770fd504d1e94abbc510c4f44c4bc7
Nevzorov Evgeny <eugen.nevzorov@gmail.com>
2022-09-28 17:13:44 +0300

    add headers

--- Tool.RawHttpHandler/Program.cs
+++ Tool.RawHttpHandler/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,8 +20,14 @@ app.UseHttpsRedirection();
 
 app.MapPost("/txt", async (HttpRequest request) =>
 {
-    var raw = await GetRawBodyStringAsync(request);
-    Console.WriteLine(raw);
+    foreach (var header in request.Headers)
+    {
+        string headerName = header.Key;
+        string headerContent = string.Join(",", header.Value.ToArray());
+        Console.WriteLine($"{headerName}:{headerContent}");
+    }
+    var rawBody = await GetRawBodyStringAsync(request);
+    Console.WriteLine($"Body:{rawBody}");
 });
 
 app.MapPost("/bin", async (HttpRequest request) =>
@@ -28,6 +37,8 @@ app.MapPost("/bin", async (HttpRequest request) =>
 });
 
 app.Run();
+
+Console.ReadKey();
 
 async Task<string> GetRawBodyStringAsync(HttpRequest request, Encoding? encoding = null)
 {
